4e9f9106ebb3c6eb6614dd98674e1cf593fb9500
denist2322 <parksomang125@gmail.com>
2022-07-04 16:58:42 +0900

    작성자명 표기

--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -4,11 +4,20 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import com.example.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
+	@Select("""
+			SELECT A.*,
+			M.nickname AS extra__writerName
+			FROM article AS A
+			LEFT JOIN member AS M
+			ON A.memberId = M.id
+			ORDER BY A.id DESC;
+			""")
 	public List<Article> getArticles();
 
 	public Article getArticle(@Param("id") int id);
